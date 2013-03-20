4c93f680dd759ab1ba2ae08ace810faba570c606
kidd0323 <shiliang0323@163.com>
2013-03-20 20:45:06 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+	echo '<center><strong>Shi Liang Gree Assignment!</strong></center>';
+	echo '<br/><br/><br/><br/>';
+	echo '<center>Shi Liang</center>';
+	echo '<br/>';
+	echo '<center>Master, Peking University, China</center>';	
+?>
+
+
+<html>
+	<center><img src="photo.jpg" style="width:115px;height:150px;"></center>
+</html>
+
+<?php
+	echo '<br/><br/><br/><br/>';
+	echo '<center>Email:shiliang@pku.edu.cn</center>';
+	echo '<center>Tel:+86 15210146434</center>';	
+?>
